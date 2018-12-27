1947eb0c79291cefbf2b5846d43fc7adb7298d69
Paavo Paloranta <30296858+paloranta@users.noreply.github.com>
2018-12-27 22:48:44 +0200

    Modified time format

--- src/components/Clock.vue
+++ src/components/Clock.vue
@@ -1,44 +1,44 @@
 //https://codesandbox.io/s/wk0n26pko8 "Vue clock"
 <template>
   <div class="clock">
-    <span>{{ hour }}:{{ minute }}:{{ second }}</span> 
-    <span>{{ amPm }}</span> 
-    <br />   
+    <h1 style="font-weight: 700;">{{ hour }}:<span style="font-weight: 300;">{{ minute }}</span>
+      <span>:</span> <span style="font-weight: 300;">{{ second }}</span>
+    </h1>
     <span>{{ date }}</span>
   </div>
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       hour: null,
       minute: null,
       second: null,
       amPm: null,
       date: null
-    }
+    };
   },
   methods: {
     updateClock() {
       const myDate = new Date();
-      const time = myDate.toLocaleTimeString();
-      const pvm = myDate.toLocaleDateString();
-      this.date = pvm;      
-      this.hour = time.split(":")[0]
+      const time = myDate.toLocaleTimeString("fr-FR");
+      const pvm = myDate.toLocaleDateString("fi-FI");
+      this.date = pvm;
+      this.hour = time.split(":")[0];
       if (this.hour < 10) {
-        this.hour = '0' + this.hour
+        this.hour = "0" + this.hour;
       }
       this.minute = myDate.getMinutes();
       if (this.minute < 10) {
-        this.minute = '0' + this.minute
+        this.minute = "0" + this.minute;
       }
-      this.second = time.split(":")[2].split(" ")[0]
-      this.amPm = time.split(" ")[1]
+      this.second = time.split(":")[2].split(" ")[0];
+      this.amPm = time.split(" ")[1];
     }
   },
-  mounted () {
+  mounted() {
     setInterval(this.updateClock, 1000);
   }
-}
+};
 </script>
