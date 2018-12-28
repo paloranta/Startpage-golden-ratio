42108edc7a1b6cc22e7502792e54b2e332bb3385
Paavo Paloranta <30296858+paloranta@users.noreply.github.com>
2018-12-28 07:30:44 +0200

    Removed extra 0 from timestring

--- src/components/Clock.vue
+++ src/components/Clock.vue
@@ -1,8 +1,7 @@
 //https://codesandbox.io/s/wk0n26pko8 "Vue clock"
 <template>
   <div class="clock">
-    <h1 style="font-weight: 700;">{{ hour }}:<span style="font-weight: 300;">{{ minute }}</span>
-      <span>:</span> <span style="font-weight: 300;">{{ second }}</span>
+    <h1 style="font-weight: 700;">{{ hour }}:<span style="font-weight: 300;">{{ minute }}:{{ second }}</span>
     </h1>
     <span>{{ date }}</span>
   </div>
@@ -26,7 +25,7 @@ export default {
       const pvm = myDate.toLocaleDateString("fi-FI");
       this.date = pvm;
       this.hour = time.split(":")[0];
-      if (this.hour < 10) {
+      /*if (this.hour < 10) {
         this.hour = "0" + this.hour;
       }
       this.minute = myDate.getMinutes();
@@ -35,6 +34,7 @@ export default {
       }
       this.second = time.split(":")[2].split(" ")[0];
       this.amPm = time.split(" ")[1];
+      */
     }
   },
   mounted() {
